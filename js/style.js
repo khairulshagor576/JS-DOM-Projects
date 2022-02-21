5ff174c43be3d180a9051fcef182d3da1e2d3e2c
khairul islam <khairulsagor567@gmail.com>
2022-02-21 16:12:54 +0600

    Hexadecimal color generate in input field and change background color by clicking a button

--- js/style.js
+++ js/style.js
@@ -1,24 +1,61 @@
 
+/*-------------------------------------------------------
+  RGB color randomly generate and backgroundcolor change
+---------------------------------------------------------*/
+// window.onload = () =>{
+// 	main();
+// }
 
-window.onload = () =>{
+// function main ()
+// {
+//   const root = document.getElementById('root');
+//   const btn = document.getElementById('change-btn');
+
+//   btn.addEventListener("click",function(){
+//   	root.style.backgroundColor = generateRGBColor ();
+//   });
+// }
+
+// function generateRGBColor ()
+// {
+// 	const red   = Math.floor(Math.random()*255);
+// 	const green = Math.floor(Math.random()*255);
+// 	const blue  = Math.floor(Math.random()*255);
+
+// 	return `rgb(${red},${green},${blue})`;	
+// }
+
+/*-----------------------------------------
+  Hex color randomly generate input field
+--------------------------------------------*/
+
+window.onload =() =>{
+
 	main();
 }
+
 
 function main ()
 {
   const root = document.getElementById('root');
   const btn = document.getElementById('change-btn');
+  const output = document.getElementById('output');
 
-  btn.addEventListener("click",function(){
-  	root.style.backgroundColor = generateRGBColor ();
+  btn.addEventListener('click',function(){
+  	bgColor = generateHexColor();
+  	root.style.backgroundColor=bgColor;
+  	output.value = bgColor;
   });
 }
 
-function generateRGBColor ()
+function generateHexColor () 
 {
-	const red   = Math.floor(Math.random()*255);
+	const red = Math.floor(Math.random()*255);
 	const green = Math.floor(Math.random()*255);
-	const blue  = Math.floor(Math.random()*255);
+	const blue = Math.floor(Math.random()*255);
 
-	return `rgb(${red},${green},${blue})`;	
+	return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
+
+
+
